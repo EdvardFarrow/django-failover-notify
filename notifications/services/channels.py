686524b8f365cac09009ccd44536d6b1b1dac9ab
edfarrow <edvardfrrw@gmail.com>
2025-11-28 22:55:38 +0400

    fix channels.py print() -> logger()

--- notifications/services/channels.py
+++ notifications/services/channels.py
@@ -1,56 +1,64 @@
 from abc import ABC, abstractmethod
 import random
-import time
+from typing import Any
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 
 class BaseChannel(ABC):
     @abstractmethod
-    def send(self, user: str, message: str) -> bool:
+    def send(self, recipient: Any, message: str) -> bool:
         pass
 
 
 
 class EmailChannel(BaseChannel):
-    def send(self, recipient: str, message: str) -> bool:
+    def send(self, recipient: Any, message: str) -> bool:
         
-        print(f"Попытка отправки Email для {recipient.username}...")
+        logger.info(f"Попытка отправки Email для {recipient.username}...")
         
         if not recipient.email:
+            logger.warning(f"У пользователя {recipient.username} нет email")
             raise ValueError("Нет email для отправки")
         
         # Эмуляция: 30% шанс падения сервиса (показать Failover)
         if random.random() < 0.3:
-            print("Ошибка: SMTP сервер недоступен")
+            logger.error("Ошибка: SMTP сервер недоступен")
             raise ConnectionError("SMTP Timeout")
             
-        print(f"[Email отправлен] Адрес: {recipient.email} | Текст: {message}")
+        logger.info(f"[Email отправлен] Адрес: {recipient.email} | Текст: {message}")
         return True
 
 
 
 class TelegramChannel(BaseChannel):
-    def send(self, recipient: str, message: str) -> bool:
+    def send(self, recipient: Any, message: str) -> bool:
         
-        print(f"Попытка отправки в Telegram для {recipient.username}...")
+        logger.info(f"Попытка отправки в Telegram для {recipient.username}...")
         
         if not recipient.telegram_id:
-            raise ValueError(f"У пользователя {recipient.username} нет Telegram ID")
+            logger.warning(f"У пользователя {recipient.username} нет Telegram ID")
+            raise ValueError("Не указан Telegram ID")
         
         if random.random() < 0.5:
-            print("Ошибка: Telegram API не отвечает (Timeout)")
+            logger.error("Ошибка: Telegram API не отвечает (Timeout)")
             raise ConnectionError("Telegram API Timeout")
 
-        print(f"[Telegram отправлен] ID: {recipient.telegram_id} | Текст: {message}")
+        logger.info(f"[Telegram отправлен] ID: {recipient.telegram_id} | Текст: {message}")
         return True
     
     
 class SMSChannel(BaseChannel):
-    def send(self, recipient: str, message: str) -> bool:
+    def send(self, recipient: Any, message: str) -> bool:
         
-        print(f"Попытка отправки SMS для {recipient.username}...")
+        logger.info(f"Попытка отправки SMS для {recipient.username}...")
         
         if not recipient.phone:
+            logger.warning(f"У пользователя {recipient.username} не указан телефон")
             raise ValueError("Нет телефона")
 
         # SMS считаем самым надежным, без рандомных ошибок
-        print(f"[SMS отправлено] Телефон: {recipient.phone} | Текст: {message}")
+        logger.info(f"[SMS отправлено] Телефон: {recipient.phone} | Текст: {message}")
         return True
